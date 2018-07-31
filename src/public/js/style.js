427e47d3e78f7cbc3b2b311e52875a53db6d0a22
nwalter <walter.nicolas@gmx.de>
2018-07-31 09:43:48 +0200

    sticky header angepasst

--- src/public/js/style.js
+++ src/public/js/style.js
@@ -13,14 +13,22 @@ function closeWin() {
 document.addEventListener('DOMContentLoaded', function () {
     window.onscroll = function() {myFunction()};
 
+    var pages = document.getElementsByClassName('page');
     var navTabs = document.getElementById('nav-tabs');
     var sticky = navTabs.offsetTop;
 
+    function setMargin(str) {
+        for (var i = 0; i < pages.length; i++) {
+            pages[i].style.marginTop = str;
+        }
+    }
     function myFunction() {
         if (window.pageYOffset > sticky) {
             navTabs.style.position = 'fixed';
+            setMargin('47px');
         } else {
             navTabs.style.position = 'static';
+            setMargin('0');
         }
     }
 });
