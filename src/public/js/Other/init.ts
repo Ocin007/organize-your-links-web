7b900301345624e802c272c26098e04261aa4c6f
nwalter <walter.nicolas@gmx.de>
2018-08-04 01:43:07 +0200

    navigation zwischen den Listen implementiert

--- src/public/js/Other/init.ts
+++ src/public/js/Other/init.ts
@@ -1,14 +1,60 @@
 let serverData;
 let playlist;
+let watched;
+let notWatched;
 
 document.addEventListener('DOMContentLoaded', function () {
     serverData = new ServerData();
     const playlistElement = document.getElementById('playlist');
-
+    const watchedElement = document.getElementById('watched');
+    const notWatchedElement = document.getElementById('not-watched');
 
     playlist = new PageList(ListID.PLAYLIST, playlistElement, serverData);
+    watched = new PageList(ListID.WATCHED, watchedElement, serverData);
+    notWatched = new PageList(ListID.NOT_WATCHED, notWatchedElement, serverData);
     serverData.get(function () {
         playlist.generateMap();
         playlist.renderList();
+
+        watched.hideElement();
+        watched.generateMap();
+        watched.renderList();
+
+        notWatched.hideElement();
+        notWatched.generateMap();
+        notWatched.renderList();
+    });
+    const tabWatched = document.getElementById('tab-watched');
+    tabWatched.addEventListener('click', function () {
+        watched.showElement();
+        playlist.hideElement();
+        notWatched.hideElement();
+        if(!tabWatched.classList.contains('tab-active')) {
+            tabWatched.classList.add('tab-active');
+        }
+        tabPlaylist.classList.remove('tab-active');
+        tabNotWatched.classList.remove('tab-active');
+    });
+    const tabPlaylist = document.getElementById('tab-playlist');
+    tabPlaylist.addEventListener('click', function () {
+        watched.hideElement();
+        playlist.showElement();
+        notWatched.hideElement();
+        tabWatched.classList.remove('tab-active');
+        if(!tabPlaylist.classList.contains('tab-active')) {
+            tabPlaylist.classList.add('tab-active');
+        }
+        tabNotWatched.classList.remove('tab-active');
+    });
+    const tabNotWatched = document.getElementById('tab-not-watched');
+    tabNotWatched.addEventListener('click', function () {
+        watched.hideElement();
+        playlist.hideElement();
+        notWatched.showElement();
+        tabWatched.classList.remove('tab-active');
+        tabPlaylist.classList.remove('tab-active');
+        if(!tabNotWatched.classList.contains('tab-active')) {
+            tabNotWatched.classList.add('tab-active');
+        }
     });
 });
--- src/public/js/polyfills.js
+++ src/public/js/polyfills.js
@@ -361,13 +361,58 @@ document.addEventListener('DOMContentLoaded', function () {
 //# sourceMappingURL=style.js.map
 var serverData;
 var playlist;
+var watched;
+var notWatched;
 document.addEventListener('DOMContentLoaded', function () {
     serverData = new ServerData();
     var playlistElement = document.getElementById('playlist');
+    var watchedElement = document.getElementById('watched');
+    var notWatchedElement = document.getElementById('not-watched');
     playlist = new PageList(ListID.PLAYLIST, playlistElement, serverData);
+    watched = new PageList(ListID.WATCHED, watchedElement, serverData);
+    notWatched = new PageList(ListID.NOT_WATCHED, notWatchedElement, serverData);
     serverData.get(function () {
         playlist.generateMap();
         playlist.renderList();
+        watched.hideElement();
+        watched.generateMap();
+        watched.renderList();
+        notWatched.hideElement();
+        notWatched.generateMap();
+        notWatched.renderList();
+    });
+    var tabWatched = document.getElementById('tab-watched');
+    tabWatched.addEventListener('click', function () {
+        watched.showElement();
+        playlist.hideElement();
+        notWatched.hideElement();
+        if (!tabWatched.classList.contains('tab-active')) {
+            tabWatched.classList.add('tab-active');
+        }
+        tabPlaylist.classList.remove('tab-active');
+        tabNotWatched.classList.remove('tab-active');
+    });
+    var tabPlaylist = document.getElementById('tab-playlist');
+    tabPlaylist.addEventListener('click', function () {
+        watched.hideElement();
+        playlist.showElement();
+        notWatched.hideElement();
+        tabWatched.classList.remove('tab-active');
+        if (!tabPlaylist.classList.contains('tab-active')) {
+            tabPlaylist.classList.add('tab-active');
+        }
+        tabNotWatched.classList.remove('tab-active');
+    });
+    var tabNotWatched = document.getElementById('tab-not-watched');
+    tabNotWatched.addEventListener('click', function () {
+        watched.hideElement();
+        playlist.hideElement();
+        notWatched.showElement();
+        tabWatched.classList.remove('tab-active');
+        tabPlaylist.classList.remove('tab-active');
+        if (!tabNotWatched.classList.contains('tab-active')) {
+            tabNotWatched.classList.add('tab-active');
+        }
     });
 });
 //# sourceMappingURL=init.js.map
