22f73c3a65f5d47feab9e521c93bf43393028f4b
Ocin007 <ocin007-github@gmx.de>
2020-09-08 01:14:05 +0200

    #30 fix installation scripts

--- src/scripts/install-dev.cmd
+++ src/scripts/install-dev.cmd
@@ -1,7 +1,2 @@
 @echo off
-cd ../..
-composer install --no-dev
-cd src/scripts
-php prepare-data-dir.php
-php combine-all-js-to-one.php
-pause
+cd ../.. && composer install && cd src/scripts && php prepare-data-dir.php && php combine-all-js-to-one.php && pause
