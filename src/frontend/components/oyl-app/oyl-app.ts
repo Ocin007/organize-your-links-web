ce6f4c340b6d6d42fcee2fbfbf5e7b58162a199f
Ocin007 <ocin007-github@gmx.de>
2020-11-18 00:21:56 +0100

    #13 add references to child elements as properties

--- src/frontend/components/oyl-app/oyl-app.ts
+++ src/frontend/components/oyl-app/oyl-app.ts
@@ -1,8 +1,19 @@
 import AbstractOylApp from "./AbstractOylApp";
+import OylNavigation from "./oyl-navigation/oyl-navigation";
+import OylPageFrame from "./oyl-page-frame/oyl-page-frame";
+import OylSlidePage from "./oyl-slide-page/oyl-slide-page";
+import OylPopupFrame from "./oyl-popup-frame/oyl-popup-frame";
+import OylNotification from "./oyl-notification/oyl-notification";
 
 class OylApp extends AbstractOylApp {
 
     static readonly tagName = 'oyl-app';
+
+    protected navigation: OylNavigation;
+    protected pageFrame: OylPageFrame;
+    protected slidePage: OylSlidePage;
+    protected popupFrame: OylPopupFrame;
+    protected notification: OylNotification;
 
     static get observedAttributes() {
         return [];
