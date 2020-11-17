94a80822d7f18cade20ddc2147a603b02a6295cb
Ocin007 <ocin007-github@gmx.de>
2020-11-17 23:40:07 +0100

    #13 implement ServiceProvider as singleton

--- src/frontend/services/ServiceProvider.ts
+++ src/frontend/services/ServiceProvider.ts
@@ -1,5 +1,16 @@
 class ServiceProvider {
 
+    private static _instance: ServiceProvider;
+
+    private constructor() {
+    }
+
+    static get instance(): ServiceProvider {
+        if (!this._instance) {
+            this._instance = new ServiceProvider();
+        }
+        return this._instance;
+    }
 }
 
 export default ServiceProvider;
