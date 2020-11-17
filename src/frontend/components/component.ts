bd97077d1d8503f43c5843720e261ed5f061c951
Ocin007 <ocin007-github@gmx.de>
2020-11-17 02:53:18 +0100

    #13 bind html elements of template to properties
    Every element with an attribute 'data-property'
    gets bound to the property specified in the
    value of 'data-property'.

--- src/frontend/components/component.ts
+++ src/frontend/components/component.ts
@@ -1,25 +1,40 @@
 class Component extends HTMLElement {
 
-    protected static _tagName = '';
-
-    static get observedAttributes() {
-        return [];
-    }
-
-    get customComponents(): Component[] {
-        return [];
-    }
-
-    protected constructor(html: string = '', scss: string = '') {
+    protected constructor(html: string = '', css: string = '') {
         super();
+        let style = Component.createStyleElement(css)
+        let template = Component.createTemplateElement(html);
+        this.initShadowRoot(style, template);
+        this.bindElementsToProperties();
+    }
+
+    private static createStyleElement(css: string): HTMLStyleElement {
         let style = document.createElement('style');
         style.setAttribute('type', 'text/css');
-        style.appendChild(document.createTextNode(scss));
+        style.appendChild(document.createTextNode(css));
+        return style;
+    }
+
+    private static createTemplateElement(html: string): HTMLTemplateElement {
         let template = document.createElement('template');
         template.innerHTML = html;
+        return template;
+    }
+
+    private initShadowRoot(style: HTMLStyleElement, template: HTMLTemplateElement) {
         this.attachShadow({mode: "open"});
         this.shadowRoot.appendChild(style);
         this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    private bindElementsToProperties() {
+        this.shadowRoot
+            .querySelectorAll<HTMLElement>('[data-property]')
+            .forEach(element => this[element.dataset.property] = element);
+    }
+
+    static get observedAttributes() {
+        return [];
     }
 
     connectedCallback(): void {
