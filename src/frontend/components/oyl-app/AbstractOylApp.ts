56988259293f1742553c90fd02df13f53481715e
Ocin007 <ocin007-github@gmx.de>
2020-11-18 00:42:50 +0100

    #13 put properties in abstract class

--- src/frontend/components/oyl-app/AbstractOylApp.ts
+++ src/frontend/components/oyl-app/AbstractOylApp.ts
@@ -4,10 +4,21 @@ import Component from "../component";
 import ControllerComponentInterface from "../ControllerComponentInterface";
 import ControllerInterface from "../ControllerInterface";
 import OylAppController from "./OylAppController";
+import OylNavigation from "./oyl-navigation/oyl-navigation";
+import OylPageFrame from "./oyl-page-frame/oyl-page-frame";
+import OylSlidePage from "./oyl-slide-page/oyl-slide-page";
+import OylPopupFrame from "./oyl-popup-frame/oyl-popup-frame";
+import OylNotification from "./oyl-notification/oyl-notification";
 
 class AbstractOylApp extends Component implements ControllerComponentInterface {
 
     private readonly _controller: ControllerInterface;
+
+    protected navigation: OylNavigation;
+    protected pageFrame: OylPageFrame;
+    protected slidePage: OylSlidePage;
+    protected popupFrame: OylPopupFrame;
+    protected notification: OylNotification;
 
     get controller(): ControllerInterface {
         return this._controller;
