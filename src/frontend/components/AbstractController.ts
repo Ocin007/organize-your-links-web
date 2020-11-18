771173bd8617be58e7e193f59f637591965e5262
Ocin007 <ocin007-github@gmx.de>
2020-11-18 18:39:09 +0100

    #13 add PageProvider to AbstractController

--- src/frontend/components/AbstractController.ts
+++ src/frontend/components/AbstractController.ts
@@ -1,11 +1,14 @@
 import ServiceProvider from "../services/ServiceProvider";
+import PageProvider from "./pages/PageProvider";
 
 class AbstractController {
 
     protected readonly services: ServiceProvider;
+    protected readonly pages: PageProvider;
 
     protected constructor() {
         this.services = ServiceProvider.instance;
+        this.pages = PageProvider.instance;
     }
 }
 
--- src/frontend/components/pages/PageProvider.ts
+++ src/frontend/components/pages/PageProvider.ts
@@ -1,5 +1,16 @@
 class PageProvider {
 
+    private static _instance: PageProvider;
+
+    private constructor() {
+    }
+
+    static get instance(): PageProvider {
+        if (!this._instance) {
+            this._instance = new PageProvider();
+        }
+        return this._instance;
+    }
 }
 
 export default PageProvider;
