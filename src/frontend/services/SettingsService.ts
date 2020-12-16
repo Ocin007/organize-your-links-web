dcf6c28927f7b32c613fc430861d03b20223f92d
Ocin007 <ocin007-github@gmx.de>
2020-12-16 09:07:07 +0100

    #13 create ApiClient, implement SettingsService

--- src/frontend/services/SettingsService.ts
+++ src/frontend/services/SettingsService.ts
@@ -1,9 +1,19 @@
 import AbstractService from "./AbstractService";
-import {Status} from "../@types/enums";
+import { Status } from "../@types/enums";
+import Observable from "../utils/Observable";
+import Observer from "../utils/Observer";
+import ServiceInterface from "./ServiceInterface";
 
-class SettingsService extends AbstractService {
+class SettingsService extends AbstractService implements ServiceInterface, Observable<SettingKey[]> {
+
+    //TODO: neue route anlegen
+    private static ROUTE = "/settings-v2";
 
     private static _instance: SettingsService;
+
+    private settings: Settings = new Map<SettingKey, any>();
+    private subs: { observer: Observer, watch?: SettingKey[] }[] = [];
+    private _isInitialised: boolean = false;
 
     private constructor() {
         super();
@@ -14,6 +24,126 @@ class SettingsService extends AbstractService {
             this._instance = new SettingsService();
         }
         return this._instance;
+    }
+
+    get isInitialised(): boolean {
+        return this._isInitialised;
+    }
+
+    async init(): Promise<void> {
+        let result = await this.api.get(SettingsService.ROUTE);
+        if (result.error !== undefined || result.response === undefined) {
+            throw new Error('SettingsService: initialisation failed.');
+        }
+        this.settings = this.objectToSettings(result.response);
+        this._isInitialised = true;
+        this.notifySubs(this.settings);
+    }
+
+    subscribe(observer: Observer, watch?: SettingKey[]): void {
+        let newEntity: { observer: Observer, watch?: SettingKey[] } = {observer: observer};
+
+        //TODO: braucht man das?
+        if (watch !== undefined) {
+            newEntity.watch = watch;
+        }
+        if (this.isInitialised) {
+            observer.update(this.getSettings(watch));
+        }
+        this.subs.push(newEntity);
+    }
+
+    unsubscribe(observer: Observer, watch?: SettingKey[]): void {
+        let index = this.subs.findIndex((value => value.observer === observer && value.watch === watch));
+        if (index === -1) {
+            this.sendDebugCannotFindObserver(observer, watch);
+            return;
+        }
+        this.subs.splice(index);
+    }
+
+    getSettings(keyList?: SettingKey[]): Settings {
+        if (!this.isInitialised) {
+            throw new Error('SettingsService: Tried to read settings before initialisation.');
+        }
+        if (keyList === undefined) {
+            return this.settings;
+        }
+        let subSettings = new Map<SettingKey, any>();
+        keyList.forEach((key => {
+            subSettings.set(key, this.settings.get(key));
+        }));
+        return subSettings;
+    }
+
+    private notifySubs(newSettings: Settings): void {
+        this.subs.forEach((sub) => {
+            if (sub.watch === undefined) {
+                sub.observer.update<Settings>(this.settings);
+                return;
+            }
+            let hasAnyKey = false;
+            sub.watch.forEach(key => hasAnyKey = hasAnyKey || newSettings.has(key));
+            if (hasAnyKey) {
+                sub.observer.update<Settings>(this.getSettings(sub.watch));
+            }
+        });
+    }
+
+    private settingsToObject(settings: Settings): object {
+        let object = {};
+        settings.forEach((value, key) => {
+            if (Array.isArray(key)) {
+                object = this.setNestedValue(key, value, object);
+            } else {
+                object[key] = value;
+            }
+        });
+        return object;
+    }
+
+    private objectToSettings(object: object, previousKeys: string[] = [], settings: Settings = new Map<SettingKey, any>()): Settings {
+        for (let key in object) {
+            if (!object.hasOwnProperty(key)) {
+                continue;
+            }
+            if (typeof object[key] === "object") {
+                settings = this.objectToSettings(object[key], previousKeys.concat(key), settings);
+            } else {
+                let settingKey: SettingKey;
+                if (previousKeys.length === 0) {
+                    settingKey = SettingsService.strToSettingKey(key);
+                } else {
+                    settingKey = SettingsService.strToSettingKey(previousKeys.concat(key));
+                }
+                settings.set(settingKey, object[key]);
+            }
+        }
+        return settings;
+    }
+
+    private static strToSettingKey(key: any): SettingKey {
+        return key;
+    }
+
+    private setNestedValue(keyList: string[], value: any, object: object = {}): object {
+        let key = keyList.shift();
+        if (keyList.length === 0) {
+            object[key] = value;
+            return object;
+        }
+        object[key] = this.setNestedValue(keyList, value, object[key]);
+        return object;
+    }
+
+    async setSettings(changed: Settings): Promise<boolean> {
+        //TODO: async mit Promise? welcher typ
+        // check isInitialised?
+        debugger;
+        let newSettings = new Map<SettingKey, any>([...this.settings, ...changed]);
+        let data = this.settingsToObject(newSettings);
+        // let response = await this.api.put(SettingsService.ROUTE, data);
+        return true;
     }
 
     //TODO: implement SettingsService
@@ -37,6 +167,12 @@ class SettingsService extends AbstractService {
 
     setNotify(status: Status, settings: {visible?: boolean, autoClose?: boolean, interval?: number}) {
 
+    }
+
+    private sendDebugCannotFindObserver(observer: Observer, watch?: SettingKey[]) {
+        this.notifier.debug('SettingsService: Given observer is not subscribed to the given list of items.', {
+            observer: observer, watch: watch
+        });
     }
 }
 
---
+++ src/frontend/utils/ApiClient.ts
@@ -0,0 +1,93 @@
+import Notifier from "./Notifier";
+
+class ApiClient {
+
+    private static _instance: ApiClient;
+    private static readonly baseApiUrl = '../api';
+    private totalRequestCount: number = 0;
+    private debugRequestCountLabel: HTMLElement;
+    private notifier: Notifier;
+
+    private constructor() {
+        this.notifier = new Notifier();
+    }
+
+    //TODO: evtl kein singleton, ist eigentlich nicht nötig
+    static get instance(): ApiClient {
+        if (!this._instance) {
+            this._instance = new ApiClient();
+        }
+        return this._instance;
+    }
+
+    get(route: string): Promise<any> {
+        return this.request('GET', route);
+    }
+
+    put(route: string, data?: object): Promise<any> {
+        return this.request('PUT', route, data);
+    }
+
+    post(route: string, data?: object): Promise<any> {
+        return this.request('POST', route, data);
+    }
+
+    delete(route: string, data?: object): Promise<any> {
+        return this.request('DELETE', route, data);
+    }
+
+    private request(method: 'GET' | 'PUT' | 'POST' | 'DELETE', route: string, data?: object): Promise<any> {
+
+        //TODO: notifications wieder buggy (wieder normal, wenn alle geschlossen mit button)
+        this.debugInitTotalRequestCount();
+        this.debugIncRequestCount();
+        this.notifier.debug(`ApiClient: Request ${method} ${route}`, data);
+
+
+        //TODO: hier evtl nur debug, der aufrufende macht dann richtige messages
+        return fetch(ApiClient.baseApiUrl + route, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data === undefined ? undefined : JSON.stringify(data)
+        }).then(res => {
+            if (res.ok) {
+                return res;
+            }
+            throw new Error(res.status + ' ' + res.statusText);
+        }).then(res => {
+            return res.json();
+        }).then(res => {
+            if (res.error !== undefined && Array.isArray(res.error)) {
+                res.error.forEach((error: string) => {
+                    //TODO: je nach statuscode warning oder error
+                    this.notifier.warn('ApiClient Server: ' + error, res);
+                });
+            }
+            this.notifier.debug(`ApiClient: Response for ${method} ${route} received.`, res);
+            return res;
+        }).catch(error => {
+            this.notifier.error('ApiClient ' + error.toString(), error);
+            return error;
+        });
+    }
+
+    private debugInitTotalRequestCount(): void {
+        if (this.totalRequestCount !== 0) {
+            return;
+        }
+        let label = document.createElement('oyl-label');
+        label.setAttribute('label', 'Total amount of requests: {{val1}}');
+        label.setAttribute('val1', '0');
+        this.debugRequestCountLabel = label;
+        this.notifier.debug(this.debugRequestCountLabel);
+    }
+
+    private debugIncRequestCount() {
+        this.totalRequestCount++;
+        this.debugRequestCountLabel.setAttribute('val1', this.totalRequestCount.toString());
+    }
+}
+
+export default ApiClient;
