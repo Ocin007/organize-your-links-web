1bec56de7aa9174d2c6182f54cbe8e74b4a53600
Ocin007 <ocin007-github@gmx.de>
2020-12-28 19:03:31 +0100

    #13 implement oyl-notify-details

--- src/frontend/components/oyl-app/oyl-notification/oyl-notify-card/oyl-notify-details/oyl-notify-details.ts
+++ src/frontend/components/oyl-app/oyl-notification/oyl-notify-card/oyl-notify-details/oyl-notify-details.ts
@@ -2,12 +2,24 @@ import html from "./oyl-notify-details.html";
 import scss from "./oyl-notify-details.scss";
 import {ComponentConnected, ComponentDisconnected, OylComponent} from "../../../../../decorators/decorators";
 import Component from "../../../../component";
+import { Status } from "../../../../../@types/enums";
+import OylDate from "../../../../common/oyl-date/oyl-date";
+import OylJson from "../../../../common/oyl-json/oyl-json";
 
 @OylComponent({
     html: html,
     scss: scss
 })
 class OylNotifyDetails extends Component {
+
+    protected statusElement: HTMLSpanElement;
+    protected msgElement: HTMLSpanElement;
+    protected timeElement1: OylDate;
+    protected timeElement2: OylDate;
+    protected htmlDetailsContainer: HTMLLIElement;
+    protected htmlDetails: HTMLSpanElement;
+    protected rawDetailsContainer: HTMLLIElement;
+    protected rawDetails: HTMLSpanElement;
 
     static get tagName(): string {
         return 'oyl-notify-details';
@@ -17,8 +29,18 @@ class OylNotifyDetails extends Component {
         return [];
     }
 
+    constructor(
+        private status: Status,
+        private msg: Node | string,
+        private date: Date,
+        private details: NotifyDetails | null
+    ) {
+        super();
+    }
+
     @ComponentConnected()
     connectedCallback(): void {
+        this.render();
     }
 
     attributeChangedCallback(name: string, oldVal: string, newVal: string): void {
@@ -26,6 +48,29 @@ class OylNotifyDetails extends Component {
 
     @ComponentDisconnected()
     disconnectedCallback(): void {
+    }
+
+    private render() {
+        this.statusElement.classList.add(this.status);
+        this.statusElement.innerText = this.status;
+        if (this.msg instanceof Node) {
+            this.msgElement.appendChild(this.msg);
+        } else {
+            this.msgElement.innerText = this.msg;
+        }
+        this.timeElement1.setAttribute('timestamp', this.date.getTime().toString());
+        this.timeElement2.setAttribute('timestamp', this.date.getTime().toString());
+        if (this.details === null) {
+            return;
+        }
+        if (this.details.html !== undefined) {
+            this.htmlDetailsContainer.classList.remove('hide');
+            this.htmlDetails.appendChild(this.details.html);
+        }
+        if (this.details.raw !== undefined) {
+            this.rawDetailsContainer.classList.remove('hide');
+            this.rawDetails.appendChild(new OylJson(this.details.raw));
+        }
     }
 }
 
